e3d071143c19a79b777c81ff8423aab882f26c8c
Dmitriy Kovalenko <dmitriy@z9.(none)>
2012-05-05 00:26:48 +0300

    Add blackfriday

--- src/main.go
+++ src/main.go
@@ -1,25 +1,32 @@
-// Copyright 2012 Dmitriy Kovalenko runningmaster.gmail.com. All rights reserved.
+// Copyright 2012 Dmitriy Kovalenko (runningmaster.gmail.com). All rights reserved.
 
 package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	//"path/filepath"
+	"github.com/russross/blackfriday"
 	"time"
 )
 
-type Post struct {
+var (
+	outdir *string = flag.String("out", "./../.out/", "output directory")
+)
+
+type post struct {
 	Name string
 	Date string
 	File string
-	Body string
+	Body []byte
 }
 
-type Posts []Post
+type posts []*post
 
-func (p *Posts) InitFromFile(filename string) {
+func (p *posts) initFromFile(filename string) {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -30,54 +37,53 @@ func (p *Posts) InitFromFile(filename string) {
 	}
 }
 
-func (p *Posts) UnMarkdown() {
+func (p *posts) unMarkdown() {
 	for _, post := range *p {
 		fmt.Println(post)
 	}
 }
 
-func (p *Posts) SaveSiteHTML() {
-	for _, post := range *p {
-		CreatePostsHTML(post)
-	}
-	CreateIndexHTML(p)
-}
-
-func CreateIndexHTML(posts *Posts) {
+func createIndexHTML(posts *posts) {
 	// process template
-	err := ioutil.WriteFile("./../.out/"+"index.html", []byte(fmt.Sprint(posts)), os.ModePerm)
+	err := ioutil.WriteFile(*outdir+"index.html", []byte(fmt.Sprint(*posts)), os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func CreatePostsHTML(post Post) {
-	// unmarkdown
+func createPostsHTML(post *post) {
+	body, err := ioutil.ReadFile("./../txt/" + post.File + ".md")
+	if err != nil {
+		panic(err)
+	}
+	post.Body = blackfriday.MarkdownCommon(body)
 	// process template
-	err := ioutil.WriteFile("./../.out/"+post.File, []byte(post.Body), os.ModePerm)
+	err = ioutil.WriteFile(*outdir+post.File, post.Body, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(post.Name)
 }
 
-func GoTextToBlog() {
-	posts := make(Posts, 1)
-	posts.InitFromFile("./../txt/index.json")
-	posts.SaveSiteHTML()
+func goTextToBlog() {
+	err := os.MkdirAll(*outdir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	posts := make(posts, 0)
+	posts.initFromFile("./../txt/index.json")
+
+	for _, post := range posts {
+		createPostsHTML(post)
+	}
+	createIndexHTML(&posts)
 }
 
 func main() {
 	t0 := time.Now()
-	err := os.RemoveAll("./../.out/")
-	if err != nil {
-		panic(err)
-	}
-	err = os.Mkdir("./../.out/", os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-	GoTextToBlog()
+	flag.Parse()
+	goTextToBlog()
 	t1 := time.Now()
 	fmt.Printf("Elapsed time %s\n", t1.Sub(t0))
 }
